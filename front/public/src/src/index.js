d7df4e9c6cd9969f7f3439eddbf681e364d5d23a
RecountsXxx <bogdankapriyanua@gmail.com>
2024-03-05 21:13:34 +0200

    added strict mode

--- front/public/src/src/index.js
+++ front/public/src/src/index.js
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
 
